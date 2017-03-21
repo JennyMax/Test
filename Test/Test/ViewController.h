929af9e02a4225c17172539bdf9f4a30daaf3b5d
JennyY <yuanjiegogo@163.com>
2017-03-21 14:35:41 +0800

    2:35

---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by 袁杰 on 17/3/21.
+//  Copyright © 2017年 VIPLimited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test/Test/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by 袁杰 on 17/3/21.
+//  Copyright © 2017年 VIPLimited. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "OneKit.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
